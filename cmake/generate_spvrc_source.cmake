373f44aae9096d38e9e92d998b9bcd587bbe5ec4
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-10-11 11:49:01 +0200

    wrap shader source file gen within cmake script to prevent rerunning

---
+++ cmake/generate_spvrc_source.cmake
@@ -0,0 +1,39 @@
+file(WRITE "${SPVRC_SRC}"
+"#include <string_view>
+#include <cstdint>
+#include <vector>
+#include \"spvrc/spvrc.hpp\"
+")
+# include shader headers
+foreach(HEADER_PATH ${SPVRC_SPIRV_HEADERS})
+    file(APPEND "${SPVRC_SRC}" "#include \"${HEADER_PATH}\"\n")
+endforeach(HEADER_PATH)
+# add namespace and shader load function
+file(APPEND "${SPVRC_SRC}" "
+// https://stackoverflow.com/a/46711735
+constexpr uint32_t hash(std::string_view data) noexcept {
+    uint32_t hash = 5381;
+    for (const char &c : data) {
+        hash = ((hash << 5) + hash) + static_cast<unsigned char>(c);
+    }
+    return hash;
+}
+
+namespace spvrc {
+    auto load(std::string_view path) -> std::vector<uint32_t> {
+        std::vector<uint32_t> data;
+        switch (hash(path)) {
+")
+# insert shader data
+foreach(SPIRV_FILE SPIRV_SYMBOL IN ZIP_LISTS SPVRC_SPIRV_FILES SPVRC_SPIRV_SYMBOLS)
+    file(APPEND "${SPVRC_SRC}"
+"            case hash(\"${SPIRV_FILE}\"): data = { ${SPIRV_SYMBOL}, ${SPIRV_SYMBOL} + std::size(${SPIRV_SYMBOL})}; break;\n"
+    )
+endforeach(SPIRV_FILE SPIRV_SYMBOL)
+# end namespace
+file(APPEND "${SPVRC_SRC}" "
+            default: break;
+        }
+        return data;
+    }
+}")
